003a93ca5bd5581807d8aaf3161b974285e05e6d
tim-hub <tim_w@outlook.com>
2015-09-03 16:08:39 +0800

    delete ngui, use ugui

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,21 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class StartGame : MonoBehaviour {
+    public Button buttonStart;
+    public Button buttonHelp;
+    public Button buttonAbout;
+    public Button buttonQuit;
+    public Text text;
+
+
+
+    void Update(){
+
+        if(buttonStart.onClick){
+            Application.LoadLevel("main");
+        }
+    }
+
+
+
+
+
+
 
 	// Use this for initialization
-	void OnGUI () {
-
-        if(GUI.Button(new Rect(400,400,100,66),"Start")){
-            Application.LoadLevel("main");
-            //it seems that scripts between different scenes cannot connect
-
-        }
-
-        if(GUI.Button(new Rect(400,470,100,66),"Exit")){
-            Application.Quit();
-            
-        }
-	}
+//	void OnGUI () {
+//
+//        if(GUI.Button(new Rect(400,400,100,66),"Start")){
+//            Application.LoadLevel("main");
+//            //it seems that scripts between different scenes cannot connect
+//
+//        }
+//
+//        if(GUI.Button(new Rect(400,470,100,66),"Exit")){
+//            Application.Quit();
+//            
+//        }
+//	}
 	
 }
