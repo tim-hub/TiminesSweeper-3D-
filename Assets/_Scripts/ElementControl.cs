89a4e8c126131010ac9457da61620d341f171f9b
tim-hub <tim_w@outlook.com>
2016-02-29 22:54:23 +0800

    remove old build files, and want to refactory new scene

---
+++ Assets/_Scripts/ElementControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ElementControl : MonoBehaviour {
+
+    public List<Vector3> Directions =new List<Vector3>(); //to define the line's direction
+
+
+
+    void Start(){
+
+
+
+
+    }
+
+
+}
