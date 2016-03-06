afb18fa34fb872c29f553744004843eaa5b0711b
tim-hub <tim_w@outlook.com>
2016-03-06 21:20:43 +0800

    modified:   Assets/_Scripts/ElementControl.cs

--- Assets/_Scripts/ElementControl.cs
+++ Assets/_Scripts/ElementControl.cs
@@ -3,30 +3,111 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 
-public class ElementControl : MonoBehaviour ,IPointerClickHandler{
+public class ElementControl : MonoBehaviour ,IPointerClickHandler,IPointerEnterHandler,
+IPointerExitHandler{
 
     public List<Vector3> Directions =new List<Vector3>(); //to define the line's directions
     //for different shapes of objects
+    public LayerMask ElementMask;
 
 
-
-    private bool _isMined;
+    private bool _isAMine;
+    private bool _isSweepered;
     private bool _isFlagged;
 
 
-    void Start(){
+    private bool _isPointerOnTheObject;
 
 
+    private int _ownNumber;
+
+    public bool IsAMine{
+
+        get {return _isAMine; }
+        set { _isAMine = value; }
+
+
+    }
+    void Awake(){
+
+        _ownNumber = GetHowManyMinesNear();
 
 
     }
 
 
+    void Start(){
 
-    void SendALine(Vector3 direction){
+        _isSweepered = false;
+        _isFlagged = false;
+
+        _isPointerOnTheObject = false;
+
+
+    }
+
+    void Update(){
+        #if UNITY_ANDROID
+
+
+
+        #else
+
+        if (_isPointerOnTheObject){ //pointer on the object
+
+
+            if (Input.GetMouseButtonDown(1)) //right click
+            {
+                _isFlagged = true;
+
+
+            }
+
+
+
+        }
+
+        #endif
+
+    }
+
+    void ClickToSendLine(Vector3 direction){
         // cast a ray along the direction
 
+        Ray ray = new Ray(transform.position, direction);
 
+
+
+
+        //set the texture by the own number
+        GetComponent<Renderer>().sharedMaterial.mainTexture;
+
+
+    }
+
+    int GetHowManyMinesNear(){ // to calculate how many mines near this element
+        int ownNumber=0;
+
+        foreach (Vector3 direction in Directions)
+        {
+
+
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit hit;
+
+
+            if (Physics.Raycast(ray, out hit, 100f, ElementMask))
+            {
+            
+
+                if (hit.collider.gameObject.GetComponent<ElementControl>().IsAMine)
+                {
+                    ownNumber++;
+                }
+
+            }
+        }
+        return ownNumber;
 
     }
 
@@ -38,7 +119,7 @@ public class ElementControl : MonoBehaviour ,IPointerClickHandler{
 
         for (int i = 0; i < Directions.Count; i++)
         {
-            SendALine(Directions [i]);
+            ClickToSendLine(Directions [i]);
 
 
 
@@ -47,4 +128,17 @@ public class ElementControl : MonoBehaviour ,IPointerClickHandler{
 
     }
 
+    public void OnPointerEnter(PointerEventData eventData){
+
+        _isPointerOnTheObject = true;
+
+    }
+
+
+    public void OnPointerExit(PointerEventData eventData){
+        _isPointerOnTheObject = false;
+
+
+
+    }
 }
