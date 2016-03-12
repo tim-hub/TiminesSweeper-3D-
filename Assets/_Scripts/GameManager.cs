47e46719f5570eb749533996ca4936f67a640839
tim-hub <tim_w@outlook.com>
2016-03-12 21:03:29 +0800

    instantiate gamemanager to manager the elements matrix

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
@@ -7,8 +8,15 @@ public class GameManager : MonoBehaviour {
 
     public static GameManager instance;
 
+    public GameObject ParentOfElements;
+    public GameObject ElementObject;
 
 
+    public float DistanceOfTwoElements=2f;
+    public int ElementsQuantity=64;
+
+
+    private List<Vector3> _elementPositions=new List<Vector3>(); 
 
     void Awake(){
 
@@ -25,8 +33,64 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        SetPositionMatrix();
+
+        SetElementsPosition();
 	
 	}
+
+    void SetPositionMatrix(){
+        int cows =(int) Mathf.Pow(ElementsQuantity, (1f / 3));
+
+        for (int i = 0; i < cows; i++)
+        {
+
+            for (int j = 0; j < cows; j++)
+            {
+                for (int k = 0; k <= cows; k++)
+                {
+
+                    _elementPositions.Add
+                    (new Vector3(i * DistanceOfTwoElements,
+                        j * DistanceOfTwoElements,
+                        k * DistanceOfTwoElements));
+
+                }
+
+            }
+
+
+        }
+    }
+
+    void SetElementsPosition(){
+        foreach (Vector3 pos in _elementPositions)
+        {
+
+            GameObject element = Instantiate(ElementObject, pos,Quaternion.identity)as GameObject;
+            element.transform.parent = ParentOfElements.transform;
+
+
+        }
+
+
+
+    }
+
+
+    void PrintMatrix(){
+
+        foreach (Vector3 pos in _elementPositions)
+        {
+
+            Debug.Log(pos);
+
+
+        }
+
+
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
