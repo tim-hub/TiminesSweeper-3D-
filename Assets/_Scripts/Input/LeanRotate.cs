f57a6146a73f5fd2295465b5c2a2ed8cfab33397
tim <tim_w@outlook.com>
2016-04-07 14:00:40 +0800

    get the rotate for touch screen and mouse input

--- Assets/_Scripts/Input/LeanRotate.cs
+++ Assets/_Scripts/Input/LeanRotate.cs
@@ -7,9 +7,8 @@ public class LeanRotate : MonoBehaviour {
 	public float SensitivityRotation=45f;
 	public float RotateSpeed=3f; //rotate speed
 
+	private Quaternion newQuaternion;
 
-	private float _y;
-	private float _x;
 	void OnEnable()
 	{
 		// Hook into the OnSwipe event
@@ -24,58 +23,19 @@ public class LeanRotate : MonoBehaviour {
 
 	void Start(){
 
-		_x=0;
-		_y=330;
+
+		newQuaternion=transform.rotation;
+
 	}
 
 	void Update(){
-//		float y = Mathf.Clamp(transform.rotation.y,-10,60);
-//		transform.rotation=Quaternion.Euler(new Vector3(transform.rotation.x,y,transform.rotation.z));
-		if(Input.touchCount>=1){
-			float deltaX=(Input.touches[0].deltaPosition.x);
-
-
-			if(Mathf.Abs(deltaX)>SwipeThreshold){
-
-
-				if(deltaX<0){
-
-					_y+=SensitivityRotation;
-
-				}else if(deltaX>0){
-
-					_y-=SensitivityRotation;
-				}
-
-
-			}
-
-		}
-
 
 		transform.rotation=Quaternion.Lerp
-			(transform.rotation,Quaternion.Euler(transform.rotation.x,_y,transform.rotation.z),
+			(transform.rotation,newQuaternion,
 				RotateSpeed*Time.deltaTime);
-		
 
 
 	}
-	// Update is called once per frame
-	void LateUpdate () {
-
-		// 2 d rotate in touch screen
-
-		// Get the center point of all touches
-		var center = Lean.LeanTouch.GetCenterOfFingers();
-
-		// This will rotate the current transform based on a multi finger twist gesture
-		Lean.LeanTouch.RotateObjectRelative(transform, Lean.LeanTouch.TwistDegrees, center);
-
-		// This will scale the current transform based on a multi finger pinch gesture
-		//Lean.LeanTouch.ScaleObjectRelative(transform, Lean.LeanTouch.PinchScale, center);
-	}
-
-
 
 	public void OnFingerSwipe(Lean.LeanFinger finger){
 
@@ -84,27 +44,33 @@ public class LeanRotate : MonoBehaviour {
 
 		if(swipe.magnitude>SwipeThreshold){
 
-//			if (swipe.x < -Mathf.Abs(swipe.y))
-//			{
-//				Debug.Log( "You swiped left!");
-//				_y+=SensitivityRotation;
-//
-//			}
-//
-//			if (swipe.x > Mathf.Abs(swipe.y))
-//			{
-//				_y-=SensitivityRotation;
-//			}
-//
-//			if (swipe.y < -Mathf.Abs(swipe.x))
-//			{
-//				_x+=SensitivityRotation;
-//			}
-//
-//			if (swipe.y > Mathf.Abs(swipe.x))
-//			{
-//				_x-=SensitivityRotation;
-//			}
+			if (swipe.x < -Mathf.Abs(swipe.y))
+			{
+				Debug.Log( "You swiped left!");
+				//_thisY+=SensitivityRotation;
+				newQuaternion=Quaternion.AngleAxis(90,Vector3.up)*transform.rotation;
+
+			}
+
+			if (swipe.x > Mathf.Abs(swipe.y))
+			{
+				
+				newQuaternion=Quaternion.AngleAxis(-90,Vector3.up)*transform.rotation;
+
+
+			}
+
+			if (swipe.y < -Mathf.Abs(swipe.x))
+			{
+				//_parentX+=SensitivityRotation;
+				newQuaternion=Quaternion.AngleAxis(90,Vector3.right)*transform.rotation;
+			}
+
+			if (swipe.y > Mathf.Abs(swipe.x))
+			{
+				
+				newQuaternion=Quaternion.AngleAxis(-90,Vector3.right)*transform.rotation;
+			}
 		}
 
 	}
