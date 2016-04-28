b2643e2f35fbee1860de30ac05b20a53b0dd668c
tim <tim_w@outlook.com>
2016-04-28 15:13:57 +0800

    add audio remember

---
+++ Assets/UI/Scripts/AudioRemember.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AudioRemember : MonoBehaviour {
+
+	public Slider MusicSlider;
+	public Slider EffectSlider;
+
+	// Use this for initialization
+	void Start () {
+		MusicSlider.value=PlayerPrefs.GetFloat("MusicVolume",MusicSlider.maxValue);
+		EffectSlider.value=PlayerPrefs.GetFloat("EffectVolume",EffectSlider.maxValue);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetMusicValue(float f){
+
+
+		PlayerPrefs.SetFloat("MusicVolume",f);
+
+	}
+
+
+	public void SetEffectValue(float f){
+
+
+		PlayerPrefs.SetFloat("EffectVolume",f);
+
+	}
+
+
+}
